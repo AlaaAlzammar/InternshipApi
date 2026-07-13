079ea7e603bdebd90322e5e7d53deca95c4c24e5
AlaaAlzammar <alaaelzammar421203754@gmail.com>
2026-07-13 16:32:34 +0300

    Change database provider to PostgreSQL

--- InternshipApi/Program.cs
+++ InternshipApi/Program.cs
@@ -21,7 +21,7 @@ namespace InternshipApi
             builder.Services.AddControllers();
 
             builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddIdentity<AspNetUser, IdentityRole>(options =>
             {
